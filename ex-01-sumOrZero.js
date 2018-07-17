e781fe0f9368740dea850fcdf7d08d15ee3d88bc
Alexis Olveres <alexis.olv.developer@gmail.com>
2018-07-17 09:44:03 -0500

    Commit inicial

---
+++ ex-01-sumOrZero.js
@@ -0,0 +1,30 @@
+//------------------------------------------------------------------
+// TASK 1 : sumOrZero()
+//
+//  Write a function called sumOrZero that returns zero
+//  if the sum of two numbers is negative
+//
+//------------------------------------------------------------------
+
+function sumOrZero(num1,num2){
+    var operation = num1+num2;
+    if (operation <= 0){
+        return 0;
+    } else {
+        return operation;
+    }
+}
+
+
+console.log("====== sumOrZero ======")
+
+console.assert(sumOrZero(0, 5) === 5)
+console.assert(sumOrZero(10, 10) === 20)
+console.assert(sumOrZero(30, 10) === 40)
+
+console.assert(sumOrZero(-30, 10) === 0)
+console.assert(sumOrZero(-10, -10) === 0)
+console.assert(sumOrZero(-5, 0) === 0)
+
+
+console.log("\n\n")
